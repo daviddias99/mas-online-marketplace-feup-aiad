19192a4119a7c9df3fbc9e507229df6324fc64d1
Manu <manuel.monge.coutinho@gmail.com>
2020-10-25 21:32:53 +0000

    Create Sellers and Buyers

--- src/Olx.java
+++ src/Olx.java
@@ -1,20 +1,47 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
+import src.agents.Buyer;
+import src.agents.Seller;
 
 public class Olx {
     private Runtime rt;
     private Profile p;
     private ContainerController mainContainer;
+    private List<Seller> sellers = new ArrayList<>();
+    private List<Buyer> buyers = new ArrayList<>();
+    
+
 
     public Olx() {
         this.rt = Runtime.instance();
         this.p = new ProfileImpl();
         this.mainContainer = rt.createMainContainer(p);
+
+        createSellers();
+        createBuyers();
+    }
+
+    public void createSellers(){
+        Map<String, Integer> products = new HashMap<>();
+        products.put("pc", 150);
+        for(int i = 0; i < 3; i++)
+            this.sellers.add(new Seller(products, i * 33 + 34));
+    }
+
+    public void createBuyers(){
+        List<String> products = new ArrayList<>();
+        products.add("pc");
+        this.buyers.add(new Buyer(products));
     }
 
     public static void main(String[] args) {
